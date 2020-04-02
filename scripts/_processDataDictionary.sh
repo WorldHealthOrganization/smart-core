afe39995e750619d6c174202e37e2f65443969aa
Adam Stevenson <stevenson_adam@yahoo.com>
2020-04-02 16:36:01 -0600

    Updated processDataDictionary script

--- scripts/_processDataDictionary.sh
+++ scripts/_processDataDictionary.sh
@@ -2,16 +2,20 @@
 #DO NOT EDIT WITH WINDOWS
 tooling_jar=tooling-1.0.4-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
+datadictionary_filename="DD.xlsx"
+datadictionary_sheetname="Master-1.0"
+scope="Core"
+
 set -e
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -ProcessAcceleratorKit -s=Core -pts=./input/datadictionary/DD.xlsx -dep=Master-1.0 -op=..
+	JAVA -jar $tooling -ProcessAcceleratorKit -s=$scope -pts=./input/datadictionary/$datadictionary_filename -dep=$datadictionary_sheetname -op=.
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -ProcessAcceleratorKit -s=Core -pts=./input/datadictionary/DD.xlsx -dep=Master-1.0 -op=..
+		JAVA -jar $tooling -ProcessAcceleratorKit -s=$scope -pts=./input/datadictionary/$datadictionary_filename -dep=$datadictionary_sheetname -op=.
 	else
 		echo ProcessAcceloratorKit NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
