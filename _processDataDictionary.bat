a7ac336eea3dcbcca1d2fcc3e3a4dfa5f537b3de
bryantaustin13 <bryant@alphora.com>
2020-11-20 09:26:17 -0700

    update tooling jar version

--- _processDataDictionary.bat
+++ _processDataDictionary.bat
@@ -1,6 +1,5 @@
-#!/bin/bash
-#DO NOT EDIT WITH WINDOWS
-tooling_jar=tooling-1.1.0-SNAPSHOT-jar-with-dependencies.jar
+@ECHO off
+tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
 datadictionary_filename="WHO-DD.xlsx"
 datadictionary_sheetname="Master-1.0"
