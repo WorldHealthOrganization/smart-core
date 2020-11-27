8480b189605ad64b49eba37a32394334953686b8
bryantaustin13 <bryant@alphora.com>
2020-11-27 08:13:40 -0700

    updating of scripts to latest

--- _autoPublish.bat
+++ _autoPublish.bat
@@ -2,22 +2,28 @@
 SET tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache
 SET resources_path=%~dp0input/resources
-SET ig_resource_path=input/anc-cds.xml
+SET ig_resource_path=input/core.xml
 SET fsoption=-fs http://localhost:8080/cqf-ruler-r4/fhir/
 SET publisher_jar=publisher.jar
 
     cmd /c _updatePublisher.bat /f
     ECHO Done with updatePublisher
+
     cmd /c _updateCQFTooling.bat /f
     ECHO Done with updateCQFTooling
-REM processacceleratorkit here
-    REM from _refresh.bat
-        JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -rp="%resources_path%" -ip="%ig_resource_path%" -t -d -p %fsoption%
+
+    cmd /c _processDataDictionary.bat
+    ECHO Done with ProcessAcceleratorKit
+
+    JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -rp="%resources_path%" -ip="%ig_resource_path%" -t -d -p %fsoption%
     ECHO Done with -RefreshIG
-REM TestIGOperation
+
+    JAVA -jar "%input_cache_path%\%tooling_jar%" -TestIG -ini=ig.ini -root-dir=%~dp0 -fv=4.0.1 -tcp=/Projects/connectathon/fhir401/input/tests -fs=http://localhost:8080/cqf-ruler-r4/fhir
+    ECHO Done with -TestIGOperation
+
     REM from _genonce.bat
-            JAVA -jar "%input_cache_path%\%publisher_jar%" -ig . %txoption%
-    ECHO Done with Publish IG
+    JAVA -jar "%input_cache_path%\%publisher_jar%" -ig . %txoption%
 
-
-REM PUBLISH to "web site"  copy output dir to another location
+REM TODO - PUBLISH to "web site"  copy output dir to another location
+    ECHO [94m Done with publish to web site
+    ECHO [0m
